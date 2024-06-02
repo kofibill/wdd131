bfbfbeb97aa9a6d646f5b866640e92b279dee683
kofibill <jessekim064@gmail.com>
2024-06-02 02:52:15 -0600

    begining my project

---
+++ project/script/home-page.js
@@ -0,0 +1,10 @@
+const year= document.getElementById("currentyear").textContent = new Date().getFullYear();
+let today = document.getElementById("lastmodified").textContent = new Date();
+
+const hamButton = document.querySelector('.menu');
+const navigation = document.querySelector('.navigation')
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show')
+    hamButton.classList.toggle('show')
+})
